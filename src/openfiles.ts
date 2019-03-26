e749bac00a2f373336922e2191767c46faf84ba9
Alexander Weiss <ik@alexanderweiss.nl>
2019-03-26 21:49:56 +0100

    Removed rimraf import

--- src/openfiles.ts
+++ src/openfiles.ts
@@ -2,7 +2,6 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as mkdirp from 'mkdirp';
-import * as rimraf from 'rimraf';
 import { window, commands, Disposable, Command, workspace } from 'vscode';
 
 export class TreeItemFile extends vscode.TreeItem {
